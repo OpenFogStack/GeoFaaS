5a05eaa7a66f88aa443a009298b52bf00904ce3d
Reza <r.malek@keemail.me>
2023-08-18 13:18:14 +0200

    add funcList() to tinyFaasClient

--- GeoFaaS/src/main/kotlin/geofaas/TinyFaasClient.kt
+++ GeoFaaS/src/main/kotlin/geofaas/TinyFaasClient.kt
@@ -6,17 +6,27 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-// https://ktor.io/docs/response.html#body
+// https://ktor.io/docs/response.html
 
+//Note: management API is on the 8080 port, Thus, CANNOT run two instances on the same ip
 class TinyFaasClient (private val host: String, val port: Int) {
     private val client: HttpClient = HttpClient(CIO) { // CIO is an asynchronous coroutine-based engine
         expectSuccess = true // expectSuccess is a resp validation
         install(Logging)
      }
 
-    suspend fun call(funcname: String): HttpResponse {
-        return client.get("http://$host:$port/$funcname")
+    suspend fun call(funcname: String, data: String): HttpResponse {
+        return client.get("http://$host:$port/$funcname") //TODO: add data in call
     }
 
-    //TODO: a list of functions available
+    suspend fun funcList(): HttpResponse {
+        return client.get("http://$host:8080/list")
+    } // TODO: handle multiple line output from tinyFaaS. what is the format?
 }
+
+//suspend fun main() {
+//    val tf = TinyFaasClient("localhost", 8000)
+//    val response = tf.funcList()
+//    val b: String = response.body()
+//    println(b)
+//}
